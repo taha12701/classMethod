6e4d802019e4cf351e1b56a8d26851b095a48df8
taha12701 <108870371+taha12701@users.noreply.github.com>
2024-08-11 16:03:10 +0500

    Add files via upload

---
+++ classmethods.py
@@ -0,0 +1,17 @@
+# class student:
+#   name="Anonymous"
+
+  # def changeName(self,name):         #class method calling
+  #   self.name=name
+    
+  # @classmethod
+  # def changeName(cls,name):
+  #   cls.name=name
+# s1=student()
+# s1.changeName("Tahaa")
+# print(s1.name)
+# print(student.name)
+
+
+
+
